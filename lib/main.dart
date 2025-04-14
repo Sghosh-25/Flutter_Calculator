ec3d648661452555af91e98a8255d9c96fa2e471
Shohan Ghosh <shohanghosh3@gmail.com>
2025-04-14 12:00:42 +0530

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:my_app/app/app.dart';
+
+void main() {
+  // runApp(Container(
+  //   color: Colors.amber
+  // ));
+  runApp(
+    MyApp(),
+  );
+}
+
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'MyApp',
+//       home: Material(
+//         child: Center(child: Container(child: Text("hlw from shohan"))),
+//       ),
+//     );
+//   }
+// }
---
+++ lib/views/home_view.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Material(
+        child: Container(
+          color: const Color.fromARGB(255, 123, 7, 255),
+          child: Center(
+            child: const Text(
+              "Hello World",
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+      );
+  }
+}
